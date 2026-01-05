a2348b3bc5586eb28068b7d44fcf8d7277c88e83
Islam <gamzatovislam314@gmail.com>
2026-01-05 23:04:19 +0300

    feat: add video player component functionality. The News Page is ready.

---
+++ src/scripts/Videos.ts
@@ -0,0 +1,65 @@
+const rootSelector = "[data-js-video-player]";
+
+class VideoPlayer {
+  private rootElement: HTMLElement;
+  private videoElement: HTMLVideoElement | null;
+  private panelElement: HTMLElement | null;
+  private playButtonElement: HTMLButtonElement | null;
+
+  private readonly selectors = {
+    root: rootSelector,
+    video: "[data-js-video-player-video]",
+    panel: "[data-js-video-player-panel]",
+    playButton: "[data-js-video-player-play-button]",
+  };
+
+  private readonly stateClasses = {
+    isActive: "is-active",
+  };
+
+  constructor(rootElement: HTMLElement) {
+    this.rootElement = rootElement;
+    this.videoElement = this.rootElement.querySelector<HTMLVideoElement>(this.selectors.video);
+    this.panelElement = this.rootElement.querySelector<HTMLElement>(this.selectors.panel);
+    this.playButtonElement = this.rootElement.querySelector<HTMLButtonElement>(
+      this.selectors.playButton
+    );
+    this.bindEvents();
+  }
+
+  private onPlayButtonClick = (): void => {
+    if (!this.videoElement || !this.panelElement) return;
+
+    this.videoElement.play();
+    this.videoElement.controls = true;
+    this.panelElement.classList.remove(this.stateClasses.isActive);
+  };
+
+  private onVideoPause = (): void => {
+    if (!this.videoElement || !this.panelElement) return;
+
+    this.videoElement.controls = false;
+    this.panelElement.classList.add(this.stateClasses.isActive);
+  };
+
+  private bindEvents(): void {
+    this.playButtonElement?.addEventListener("click", this.onPlayButtonClick);
+    this.videoElement?.addEventListener("pause", this.onVideoPause);
+  }
+}
+
+// Класс для инициализации логики всех видеоплееров на одной странице.
+class VideoPlayerCollection {
+  constructor() {
+    this.init();
+  }
+
+  private init(): void {
+    const elements = document.querySelectorAll<HTMLElement>(rootSelector);
+    elements.forEach((element: HTMLElement) => {
+      new VideoPlayer(element);
+    });
+  }
+}
+
+export default VideoPlayerCollection;
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,5 +1,7 @@
 import Header from "./Header.ts";
 import TabsCollection from "./Tabs.ts";
+import VideoPlayerCollection from "./Videos.ts";
 
 new Header();
 new TabsCollection();
+new VideoPlayerCollection();
